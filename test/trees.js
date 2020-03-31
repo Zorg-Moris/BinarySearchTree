9d3d09b8b91e503381d87cea6487b1056f2da86a
Zorg-Moris <mvr.leon@gmail.com>
2020-03-31 21:15:26 +0300

    update test

--- test/trees.js
+++ test/trees.js
@@ -1,78 +1,83 @@
 module.exports.first = {
-    value: 3,
+  value: 3,
+  left: {
+    value: 5,
     left: {
-      value: 5,
+      value: 1,
+      left: undefined,
+      right: undefined,
+    },
+    right: {
+      value: 4,
+      left: undefined,
+      right: undefined,
+    },
+  },
+  right: {
+    value: 2,
+    left: {
+      value: 6,
+      left: undefined,
+      right: undefined,
+    },
+    right: undefined,
+  }
+};
+
+module.exports.second = {
+  value: 10,
+  left: {
+    value: 8,
+    left: {
+      value: 2,
       left: {
         value: 1,
         left: undefined,
         right: undefined,
       },
       right: {
-        value: 4,
-        left: undefined,
-        right: undefined,
-      },
-    },
-    right: {
-      value: 2,
-      left: {
         value: 6,
+        left: {
+          value: 4,
+          left: {
+            value: 3,
+            left: undefined,
+            right: undefined,
+          },
+          right: {
+            value: 5,
+            left: undefined,
+            right: undefined,
+          }
+        },
+        right: undefined
+      },
+    },
+    right: undefined,
+  },
+  right: {
+    value: 11,
+    left: undefined,
+    right: {
+      value: 14,
+      left: {
+        value: 13,
         left: undefined,
         right: undefined,
       },
-      right: undefined,
-    }
-  };
-  module.exports.second = {
-    value: 10,
-    left: {
-      value: 8,
-      left: {
-        value: 2,
-        left: {
-          value: 1,
-          left: undefined,
-          right: undefined,
-        },
-        right: {
-          value: 6,
-          left: {
-            value: 4,
-            left: {
-              value: 3,
-              left: undefined,
-              right: undefined,
-            },
-            right: {
-              value: 5,
-              left: undefined,
-              right: undefined,
-            }
-          },
-          right: undefined
-        },
-      },
-      right: undefined,
-    },
-    right: {
-      value: 11,
-      left: undefined,
       right: {
-        value: 14,
-        left: {
-          value: 13,
-          left: undefined,
-          right: undefined,
-        },
-        right: {
-          value: 16,
-          left: undefined,
-          right: undefined,
-        },
+        value: 16,
+        left: undefined,
+        right: undefined,
       },
-    }
-  };
-  module.exports.third = {
+    },
+  }
+};
+
+
+module.exports.third = {
+  value: 15,
+  left: {
     value: 15,
     left: {
       value: 15,
@@ -88,102 +93,39 @@ module.exports.first = {
                 value: 15,
                 left: {
                   value: 15,
-                  left: {
-                    value: 15,
-                    left: {
-                      value: 15,
-                      left: undefined,
-                      right: {
-                        value: 15,
-                        left: {
-                          value: 15,
-                          left: {
-                            value: 15,
-                            left: undefined,
-                            right: undefined,
-                          },
-                          right: {
-                            value: 15,
-                            left: undefined,
-                            right: undefined,
-                          },
-                        },
-                        right: {
-                          value: 15,
-                          left: undefined,
-                          right: {
-                            value: 15,
-                            left: undefined,
-                            right: undefined,
-                          },
-                        },
-                      },
-                    },
-                    right: undefined,
-                  },
                   right: {
-                    value: 15,
-                    left: {
-                      value: 15,
-                      left: undefined,
-                      right: {
-                        value: 15,
-                        left: undefined,
-                        right: undefined,
-                      },
-                    },
-                    right: undefined,
-                  },
-                },
-                right: undefined,
-              },
-              right: {
-                value: 15,
-                left: {
-                  value: 15,
-                  left: {
-                    value: 15,
-                    left: undefined,
-                    right: undefined,
-                  },
-                  right: undefined,
-                },
-                right: undefined,
-              },
-            },
-            right: undefined,
-          },
-          right: {
-            value: 15,
-            left: {
-              value: 15,
-              left: {
-                value: 15,
-                left: {
-                  value: 15,
-                  left: undefined,
-                  right: undefined,
-                },
-                right: {
-                  value: 15,
-                  left: {
                     value: 15,
                     left: {
                       value: 15,
                       left: {
                         value: 15,
-                        left: {
-                          value: 15,
-                          left: undefined,
-                          right: undefined,
-                        },
-                        right: undefined,
+                        left: undefined,
+                        right: undefined
                       },
-                      right: undefined,
+                      right: {
+                        value: 15,
+                        left: undefined,
+                        right: undefined
+                      }
                     },
-                    right: undefined,
+                    right: {
+                      value: 15,
+                      right: {
+                        value: 16,
+                        left: undefined,
+                        right: undefined
+                      }
+                    },
                   },
-                  right: {
+                  left: {
+                    value: 15,
+                    left: undefined,
+                    right: undefined
+                  },
+                },
+                right: {
+                  value: 15,
+                  left: {
                     value: 15,
                     left: undefined,
                     right: {
@@ -192,34 +134,96 @@ module.exports.first = {
                       right: undefined,
                     },
                   },
+                  right: undefined,
                 },
               },
               right: undefined,
             },
-            right: undefined,
+            right: {
+              value: 15,
+              left: {
+                value: 15,
+                left: {
+                  value: 15,
+                  left: undefined,
+                  right: undefined,
+                },
+                right: undefined,
+              },
+              right: undefined,
+            },
           },
+          right: undefined,
         },
         right: {
           value: 15,
-          left: undefined,
+          left: {
+            value: 15,
+            left: {
+              value: 15,
+              left: {
+                value: 15,
+                left: undefined,
+                right: undefined,
+              },
+              right: {
+                value: 15,
+                left: {
+                  value: 15,
+                  left: {
+                    value: 15,
+                    left: {
+                      value: 15,
+                      left: {
+                        value: 15,
+                        left: undefined,
+                        right: undefined,
+                      },
+                      right: undefined,
+                    },
+                    right: undefined,
+                  },
+                  right: undefined,
+                },
+                right: {
+                  value: 15,
+                  left: undefined,
+                  right: {
+                    value: 15,
+                    left: undefined,
+                    right: undefined,
+                  },
+                },
+              },
+            },
+            right: undefined,
+          },
           right: undefined,
         },
       },
       right: {
         value: 15,
-        left: {
-          value: 15,
-          left: undefined,
-          right: undefined,
-        },
-        right: {
-          value: 15,
-          left: undefined,
-          right: undefined,
-        },
+        left: undefined,
+        right: undefined,
       },
     },
     right: {
+      value: 15,
+      left: {
+        value: 15,
+        left: undefined,
+        right: undefined,
+      },
+      right: {
+        value: 15,
+        left: undefined,
+        right: undefined,
+      },
+    },
+  },
+  right: {
+    value: 15,
+    left: {
       value: 15,
       left: {
         value: 15,
@@ -233,23 +237,15 @@ module.exports.first = {
                 value: 15,
                 left: {
                   value: 15,
-                  left: {
+                  left: undefined,
+                  right: {
                     value: 15,
                     left: undefined,
                     right: {
                       value: 15,
                       left: undefined,
-                      right: {
-                        value: 15,
-                        left: undefined,
-                        right: undefined,
-                      },
+                      right: undefined,
                     },
-                  },
-                  right: {
-                    value: 15,
-                    left: undefined,
-                    right: undefined,
                   },
                 },
                 right: {
@@ -261,14 +257,22 @@ module.exports.first = {
               right: {
                 value: 15,
                 left: undefined,
-                right: {
-                  value: 15,
-                  left: undefined,
-                  right: undefined,
-                },
+                right: undefined,
               },
             },
             right: {
+              value: 15,
+              left: undefined,
+              right: {
+                value: 15,
+                left: undefined,
+                right: undefined,
+              },
+            },
+          },
+          right: {
+            value: 15,
+            left: {
               value: 15,
               left: {
                 value: 15,
@@ -276,29 +280,17 @@ module.exports.first = {
                   value: 15,
                   left: {
                     value: 15,
-                    left: {
-                      value: 15,
-                      left: undefined,
-                      right: {
-                        value: 15,
-                        left: undefined,
-                        right: {
-                          value: 15,
-                          left: {
-                            value: 15,
-                            left: undefined,
-                            right: undefined,
-                          },
-                          right: undefined,
-                        },
-                      },
-                    },
+                    left: undefined,
                     right: {
                       value: 15,
                       left: undefined,
                       right: {
                         value: 15,
-                        left: undefined,
+                        left: {
+                          value: 15,
+                          left: undefined,
+                          right: undefined,
+                        },
                         right: undefined,
                       },
                     },
@@ -306,12 +298,25 @@ module.exports.first = {
                   right: {
                     value: 15,
                     left: undefined,
-                    right: undefined,
+                    right: {
+                      value: 15,
+                      left: undefined,
+                      right: undefined,
+                    },
                   },
                 },
                 right: {
                   value: 15,
-                  left: {
+                  left: undefined,
+                  right: undefined,
+                },
+              },
+              right: {
+                value: 15,
+                left: {
+                  value: 15,
+                  left: undefined,
+                  right: {
                     value: 15,
                     left: undefined,
                     right: {
@@ -323,84 +328,74 @@ module.exports.first = {
                         right: {
                           value: 15,
                           left: undefined,
-                          right: {
-                            value: 15,
-                            left: undefined,
-                            right: undefined,
-                          },
+                          right: undefined,
                         },
                       },
                     },
                   },
-                  right: undefined,
                 },
+                right: undefined,
               },
-              right: undefined,
             },
+            right: undefined,
           },
-          right: undefined,
         },
-        right: {
-          value: 15,
-          left: undefined,
-          right: undefined,
-        },
+        right: undefined,
       },
       right: {
         value: 15,
         left: undefined,
+        right: undefined,
+      },
+    },
+    right: {
+      value: 15,
+      left: undefined,
+      right: {
+        value: 15,
+        left: {
+          value: 15,
+          left: {
+            value: 15,
+            left: undefined,
+            right: undefined,
+          },
+          right: {
+            value: 15,
+            left: undefined,
+            right: undefined,
+          },
+        },
         right: {
           value: 15,
           left: {
             value: 15,
-            left: {
-              value: 15,
-              left: undefined,
-              right: undefined,
-            },
-            right: {
-              value: 15,
-              left: undefined,
-              right: undefined,
-            },
+            left: undefined,
+            right: undefined,
           },
           right: {
             value: 15,
             left: {
               value: 15,
-              left: undefined,
+              left: {
+                value: 15,
+                left: undefined,
+                right: {
+                  value: 15,
+                  left: undefined,
+                  right: undefined,
+                },
+              },
               right: undefined,
             },
             right: {
               value: 15,
-              left: {
-                value: 15,
-                left: {
-                  value: 15,
-                  left: undefined,
-                  right: {
-                    value: 15,
-                    left: undefined,
-                    right: undefined,
-                  },
-                },
-                right: undefined,
-              },
-              right: {
-                value: 15,
-                left: undefined,
-                right: undefined,
-              },
+              left: undefined,
+              right: undefined,
             },
           },
         },
       },
-    }
-  };
-  // value:15,
-  // left: {
-  //
-  // },
-  // right: {
-  //
-  // },
+    },
+  }
+};
